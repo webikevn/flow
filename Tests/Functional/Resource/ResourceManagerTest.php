neos/flow-development-collection

BUGFIX: Allow to delete "used" resources from a storage
This solves the following case…

Given these settings:

    resource:
      collections:
        readableFilenames:
          storage: 'readableFilenameResourcesStorage'
          target: 'readableFilenameResourcesTarget'
      storages:
        readableFilenameResourcesStorage:
          storage: 'Neos\Flow\ResourceManagement\Storage\WritableFileSystemStorage'
          storageOptions:
            path: '%FLOW_PATH_DATA%Persistent/ReadableResources/'
      targets:
        readableFilenameResourcesTarget:
          target: 'Acme\AcmeCom\FilenameFileSystemSymlinkTarget'
          targetOptions:
            path: '%FLOW_PATH_WEB%Files/'
            baseUri: 'Files/'

I want to "move" a resource from the `persistent` to the `readableFilenames`
collection. To do this, I get an asset, fetch the resource and import it
into the `readableFilenames` collection. After that the newly imported
resource is published, assigned to the asset and then the old resource is
deleted. Code would be something like this:

    $resource = $asset->getResource();

    $importedResource = $resourceCollection->importResource($resource->getStream());
    $importedResource->setFilename($resource->getFilename());
    $importedResource->setMediaType($resource->getMediaType());
    $resourceCollection->getTarget()->publishResource($resource, $resourceCollection);

    $asset->setResource($importedResource);
    $this->assetRepository->update($asset);

    $this->resourceManager->deleteResource($resource);

But this leads to log messages about the storage data not being deleted,
because the resource is still being used. Which is not true, or at least
not fully correct. The problem at this point: the same resource exists
in two collections, but the check only looks at the SHA1 (and filename,
partly).

So this change adjusts the checks involved to look at the collection a
resource is in, too.

Neos.Flow/Tests/Functional/ResourceManagement/ResourceManagerTest.php
@@ -1,0 +1,93 @@
+<?php
+namespace Neos\Flow\Tests\Functional\ResourceManagement;
+
+/*
+ * This file is part of the Neos.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\Flow\Persistence\Doctrine\PersistenceManager;
+use Neos\Flow\ResourceManagement\ResourceManager;
+use Neos\Flow\ResourceManagement\ResourceRepository;
+use Neos\Flow\Tests\FunctionalTestCase;
+
+/**
+ * Functional tests for the ResourceManager
+ */
+class ResourceManagerTest extends FunctionalTestCase
+{
+    /**
+     * @var ResourceManager
+     */
+    protected $resourceManager;
+
+    /**
+     * @var ResourceRepository
+     */
+    protected $resourceRepository;
+
+    /**
+     * @var boolean
+     */
+    protected static $testablePersistenceEnabled = true;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        if (!$this->persistenceManager instanceof PersistenceManager) {
+            $this->markTestSkipped('Doctrine persistence is not enabled');
+        }
+        $this->resourceManager = $this->objectManager->get(ResourceManager::class);
+        $this->resourceRepository = $this->objectManager->get(ResourceRepository::class);
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceKeepsDataIfStillInUse()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'other-fixture.txt');
+
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceRemovesDataIfStillInUseButCollectionDiffersWithoutPersistAll()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt', 'custom');
+
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+        self::assertFileNotExists(FLOW_PATH_DATA . 'Persistent/Test/CustomResources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa');
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceRemovesDataIfStillInUseButCollectionDiffersWithPersistAll()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt', 'custom');
+
+        $this->persistenceManager->persistAll();
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+        self::assertFileNotExists(FLOW_PATH_DATA . 'Persistent/Test/CustomResources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa');
+    }
+}

Neos.Flow/Tests/FunctionalTestCase.php
@@ -437,12 +437,21 @@ abstract class FunctionalTestCase extends \Neos\Flow\Tests\BaseTestCase
     protected function cleanupPersistentResourcesDirectory()
     {
         $settings = self::$bootstrap->getObjectManager()->get(ConfigurationManager::class)->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS);
-        $resourcesStoragePath = $settings['Neos']['Flow']['resource']['storages']['defaultPersistentResourcesStorage']['storageOptions']['path'];
-        if (strpos($resourcesStoragePath, FLOW_PATH_DATA) === false) {
-            throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" but it must point to a directory below "%s". Please check the Flow settings for the Testing context.', $resourcesStoragePath, FLOW_PATH_DATA), 1382018388);
-        }
-        if (file_exists($resourcesStoragePath)) {
-            Files::removeDirectoryRecursively($resourcesStoragePath);
+        foreach ($settings['Neos']['Flow']['resource']['storages'] as $storageName => $storageSettings) {
+            if (!isset($storageSettings['storageOptions']['path'])) {
+                continue;
+            }
+
+            $resourcesStoragePath = $storageSettings['storageOptions']['path'];
+            if (strpos($resourcesStoragePath, FLOW_PATH_DATA) === false) {
+                throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" for the "%s" storage, but it must point to a directory below "%s". Please check the Flow settings for the Testing context.', $resourcesStoragePath, $storageName, FLOW_PATH_DATA), 1382018388);
+            }
+            if (strpos($resourcesStoragePath, '/Test/') === false) {
+                throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" for the "%s" storage, but it must contain "/Test/". Please check the Flow settings for the Testing context.', $resourcesStoragePath, $storageName), 1382018388);
+            }
+            if (file_exists($resourcesStoragePath)) {
+                Files::removeDirectoryRecursively($resourcesStoragePath);
+            }
         }
     }
 

webikevn/flow

          storage: 'TYPO3\Flow\ResourceManagement\Storage\WritableFileSystemStorage'

Tests/Functional/Resource/ResourceManagerTest.php
@@ -1,0 +1,93 @@
+<?php
+namespace TYPO3\Flow\Tests\Functional\ResourceManagement;
+
+/*
+ * This file is part of the TYPO3.Flow package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Persistence\Doctrine\PersistenceManager;
+use TYPO3\Flow\Resource\ResourceManager;
+use TYPO3\Flow\Resource\ResourceRepository;
+use TYPO3\Flow\Tests\FunctionalTestCase;
+
+/**
+ * Functional tests for the ResourceManager
+ */
+class ResourceManagerTest extends FunctionalTestCase
+{
+    /**
+     * @var ResourceManager
+     */
+    protected $resourceManager;
+
+    /**
+     * @var ResourceRepository
+     */
+    protected $resourceRepository;
+
+    /**
+     * @var boolean
+     */
+    protected static $testablePersistenceEnabled = true;
+
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        if (!$this->persistenceManager instanceof PersistenceManager) {
+            $this->markTestSkipped('Doctrine persistence is not enabled');
+        }
+        $this->resourceManager = $this->objectManager->get(ResourceManager::class);
+        $this->resourceRepository = $this->objectManager->get(ResourceRepository::class);
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceKeepsDataIfStillInUse()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'other-fixture.txt');
+
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceRemovesDataIfStillInUseButCollectionDiffersWithoutPersistAll()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt', 'custom');
+
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+        self::assertFileNotExists(FLOW_PATH_DATA . 'Persistent/Test/CustomResources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa');
+    }
+
+    /**
+     * @test
+     */
+    public function deleteResourceRemovesDataIfStillInUseButCollectionDiffersWithPersistAll()
+    {
+        $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt');
+        $otherResource = $this->resourceManager->importResourceFromContent('fixture', 'fixture.txt', 'custom');
+
+        $this->persistenceManager->persistAll();
+        $this->resourceManager->deleteResource($otherResource);
+
+        self::assertStringEqualsFile(FLOW_PATH_DATA . 'Persistent/Test/Resources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa', 'fixture');
+        self::assertFileNotExists(FLOW_PATH_DATA . 'Persistent/Test/CustomResources/5/1/c/f/51cff3c1f0bc59f6187e7040cc12a4e9b1eca7aa');
+    }
+}

Tests/FunctionalTestCase.php
@@ -435,13 +435,22 @@ abstract class FunctionalTestCase extends \TYPO3\Flow\Tests\BaseTestCase
      */
     protected function cleanupPersistentResourcesDirectory()
     {
-        $settings = self::$bootstrap->getObjectManager()->get(\TYPO3\Flow\Configuration\ConfigurationManager::class)->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS);
-        $resourcesStoragePath = $settings['TYPO3']['Flow']['resource']['storages']['defaultPersistentResourcesStorage']['storageOptions']['path'];
-        if (strpos($resourcesStoragePath, FLOW_PATH_DATA) === false) {
-            throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" but it must point to a directory below "%s". Please check the Flow settings for the Testing context.', $resourcesStoragePath, FLOW_PATH_DATA), 1382018388);
-        }
-        if (file_exists($resourcesStoragePath)) {
-            Files::removeDirectoryRecursively($resourcesStoragePath);
+        $settings = self::$bootstrap->getObjectManager()->get(ConfigurationManager::class)->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_SETTINGS);
+        foreach ($settings['TYPO3']['Flow']['resource']['storages'] as $storageName => $storageSettings) {
+            if (!isset($storageSettings['storageOptions']['path'])) {
+                continue;
+            }
+
+            $resourcesStoragePath = $storageSettings['storageOptions']['path'];
+            if (strpos($resourcesStoragePath, FLOW_PATH_DATA) === false) {
+                throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" for the "%s" storage, but it must point to a directory below "%s". Please check the Flow settings for the Testing context.', $resourcesStoragePath, $storageName, FLOW_PATH_DATA), 1382018388);
+            }
+            if (strpos($resourcesStoragePath, '/Test/') === false) {
+                throw new \Exception(sprintf('The storage path for persistent resources for the Testing context is "%s" for the "%s" storage, but it must contain "/Test/". Please check the Flow settings for the Testing context.', $resourcesStoragePath, $storageName), 1382018388);
+            }
+            if (file_exists($resourcesStoragePath)) {
+                Files::removeDirectoryRecursively($resourcesStoragePath);
+            }
         }
     }
 